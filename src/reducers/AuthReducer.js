7bfe5c368a2c082d237291c570815df3b053524b
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-19 18:48:57 +0530

    password state updated.

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,10 +1,18 @@
-import { EMAIL_CHANGED } from '../actions/types';
-const INITIAL_STATE = { email: '' }; 
+import { 
+	EMAIL_CHANGED, 
+	PASSWORD_CHANGED 
+} from '../actions/types';
+const INITIAL_STATE = { 
+	email: '',
+	password: ''
+}; 
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
 		case EMAIL_CHANGED:
 			return { ...state, email: action.payload }; // Create new object and update the values of state, so redux understands that there is an update in the value
+		case PASSWORD_CHANGED:
+			return { ...state, password: action.payload };
 		default:
 			return state;
 	} 
