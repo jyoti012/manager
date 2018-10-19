dd805a4bec43a074973864fb9d42e2852c2d0245
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-19 15:47:12 +0530

    Adding firebase
    Firebase installation
    npm i --global yarn
    yarn add firebase@5.3.0
    npm uninstall yarn --global

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+
+	componentWillMount() {
+		const config = {
+			apiKey: 'AIzaSyCiUUGX5yyP5XAkgQQAMnrn6kxAjIWuxfM',
+			authDomain: 'manager-37974.firebaseapp.com',
+			databaseURL: 'https://manager-37974.firebaseio.com',
+			projectId: 'manager-37974',
+			storageBucket: 'manager-37974.appspot.com',
+			messagingSenderId: '43361598201'
+		};
+		firebase.initializeApp(config);
+	}
+
   render() {
 		return(
 			<Provider store={createStore(reducers)}>
