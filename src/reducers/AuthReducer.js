665d865a4de035963a2e0a3698911557889a1d7a
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-19 18:32:17 +0530

    update (state) email when changed.

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,10 @@
+import { EMAIL_CHANGED } from '../actions/types';
 const INITIAL_STATE = { email: '' }; 
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-		
+		case EMAIL_CHANGED:
+			return { ...state, email: action.payload }; // Create new object and update the values of state, so redux understands that there is an update in the value
 		default:
 			return state;
 	} 
